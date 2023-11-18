68d3a4f649880260e772970378f288a015317030
Mayank <cmayank949@gmail.com>
2023-11-18 11:38:05 +0530

    updated production console to create product

--- production-console/src/views/Production.jsx
+++ production-console/src/views/Production.jsx
@@ -13,7 +13,7 @@ const Production = () => {
             alert("Please enter all fields!");
             return;
         }
-        let url = "";
+        let url = "http://localhost:8080/product";
         setProcessing(true);
         try {
             await axios({
@@ -21,6 +21,11 @@ const Production = () => {
                 method: "POST",
                 data: { name, qty, description, uniqueId: Date.now() },
             });
+
+            alert("Product saved!");
+            setName("");
+            setQty("");
+            setDescription("");
         } catch (error) {
             console.log(error);
             alert("Something went wrong!");
@@ -36,6 +41,7 @@ const Production = () => {
                     type="text"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
+                    placeholder="Name"
                 />
             </div>
             <div className="inputbox">
@@ -43,6 +49,7 @@ const Production = () => {
                     type="text"
                     value={qty}
                     onChange={(e) => setQty(e.target.value)}
+                    placeholder="Qty"
                 />
             </div>
             <div className="inputbox">
@@ -50,6 +57,7 @@ const Production = () => {
                     type="text"
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
+                    placeholder="Description"
                 />
             </div>
             <div className="inputbox">
